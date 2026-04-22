24da7ca81b584d1be1330df90b33982bbbef8308
David Simansky <dsimansk@redhat.com>
2026-04-22 19:44:47 +0200

    Exclude kafka-dispatcher from mesh

--- hack/lib/serverless.bash
+++ hack/lib/serverless.bash
@@ -376,10 +376,10 @@ spec:
       sidecar.istio.io/inject: "true"
     name: kafka-sink-receiver
   - labels:
-      sidecar.istio.io/inject: "true"
+      sidecar.istio.io/inject: "false"
     name: kafka-source-dispatcher
   - labels:
-      sidecar.istio.io/inject: "true"
+      sidecar.istio.io/inject: "false"
     name: kafka-controller
   - labels:
       sidecar.istio.io/inject: "false"
